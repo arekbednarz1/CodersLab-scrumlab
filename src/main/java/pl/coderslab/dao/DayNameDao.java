a5a00a159c82d88759d3745642c9793b71c1ac4a
JerzyGajewski <jerzygajewski3@gmail.com>
2020-09-28 18:56:38 +0200

    finished DayNameDao and DayName files

---
+++ src/main/java/pl/coderslab/dao/DayNameDao.java
@@ -0,0 +1,33 @@
+package pl.coderslab.dao;
+
+import pl.coderslab.model.DayName;
+import pl.coderslab.utils.DbUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DayNameDao {
+    //Zapytania SQL
+    private static final String READ_ALL_DAYS_QUERY = "SELECT * FROM day_name";
+
+    public List<DayName> readAllDays(){
+    List<DayName> dayNamesList = new ArrayList<>();
+    try(Connection conn = DbUtil.getConnection();
+        PreparedStatement statement = conn.prepareStatement(READ_ALL_DAYS_QUERY);
+        ResultSet resultSet = statement.executeQuery()){
+        while (resultSet.next()){
+            DayName dayName = new DayName();
+            dayName.setId(resultSet.getInt("id"));
+            dayName.setName(resultSet.getString("name"));
+            dayNamesList.add(dayName);
+        }
+    }catch (SQLException e){
+        e.printStackTrace();
+    }
+    return dayNamesList;
+    }
+}
---
+++ src/main/java/pl/coderslab/model/DayName.java
@@ -0,0 +1,28 @@
+package pl.coderslab.model;
+
+public class DayName {
+    private int id;
+    private String name;
+
+    public DayName(){}
+    public DayName(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
