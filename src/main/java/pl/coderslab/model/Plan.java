788a75f80ef0414da008e1dfb33c8fb7e71307b4
JerzyGajewski <jerzygajewski3@gmail.com>
2020-09-28 12:57:12 +0200

    finished PlanDao

---
+++ src/main/java/pl/coderslab/model/Plan.java
@@ -0,0 +1,60 @@
+package pl.coderslab.model;
+
+public class Plan {
+
+    private int id;
+    private String name;
+    private String description;
+    private String created;
+    private String adminId;
+
+    public Plan() {
+    }
+
+    public Plan(String name, String description, String created) {
+        this.name = name;
+        this.description = description;
+        this.created = created;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public String getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(String adminId) {
+        this.adminId = adminId;
+    }
+}
+
