20936a3fa5a5919b0dad29fab3214a0132765182
woj ojojoj <50807860+USERNAME-ALREADY-TAKENN@users.noreply.github.com>
2020-10-03 17:23:33 +0200

    hotfix: added logged in check on contact,about,allrecipes pages

--- src/main/java/pl/coderslab/web/AboutServlet.java
+++ src/main/java/pl/coderslab/web/AboutServlet.java
@@ -1,10 +1,13 @@
 package pl.coderslab.web;
 
+import pl.coderslab.model.Admins;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "AboutServlet", urlPatterns = {"/about"})
@@ -14,6 +17,14 @@ public class AboutServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        Admins admin = (Admins) session.getAttribute("admin");
+        if(admin == null) {
+            request.setAttribute("logged", false);
+        }
+        else {
+            request.setAttribute("logged", true);
+        }
         getServletContext().getRequestDispatcher("/WEB-INF/about.jsp").forward(request, response);
     }
 }
--- src/main/java/pl/coderslab/web/AllRecipesServlet.java
+++ src/main/java/pl/coderslab/web/AllRecipesServlet.java
@@ -1,12 +1,14 @@
 package pl.coderslab.web;
 
 import pl.coderslab.dao.RecipeDao;
+import pl.coderslab.model.Admins;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "AllRecipesServlet", urlPatterns = {"/allRecipes"})
@@ -16,8 +18,17 @@ public class AllRecipesServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        Admins admin = (Admins) session.getAttribute("admin");
+        if(admin == null) {
+            request.setAttribute("logged", false);
+        }
+        else {
+            request.setAttribute("logged", true);
+        }
+
         RecipeDao recipeDao = new RecipeDao();
         request.setAttribute("recipe",recipeDao.readAllRecipes());
-getServletContext().getRequestDispatcher("/WEB-INF/allRecipes.jsp").forward(request,response);
+        getServletContext().getRequestDispatcher("/WEB-INF/allRecipes.jsp").forward(request,response);
     }
 }
